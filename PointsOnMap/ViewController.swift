7ffad38a88e1fa63d885fe153840bf43d33c3d3a
THuitema <thuitema35@gmail.com>
2021-02-26 20:53:08 -0500

    Initial Commit

---
+++ PointsOnMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PointsOnMap
+//
+//  Created by Thomas Huitema on 2/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
